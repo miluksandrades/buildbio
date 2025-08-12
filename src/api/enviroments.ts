adb8eff3c31fc8231e31e219ef1d152543dff799
Lucas Andrade <luksandrades18@gmail.com>
2025-08-12 11:20:28 -0300

    remove: enviroment file

--- src/api/enviroments.ts
+++
@@ -1,4 +0,0 @@
-export const environments = {
-    prompt_default:
-        "Crie uma biografia para um perfil profissional para o LinkedIn. A biografia deve ser escrita em português e conter no máximo 200 palavras. Coloque na primeira pessoa, como se fosse o próprio usuário escrevendo. Sem formato Markdown. A biografia deve conter as seguintes informações: ",
-};
